441f01b5952378690a74c5fff5da429a4b51902f
rkuchenthal <koochenthal0@gmail.co,=m>
2021-10-26 17:37:13 -0700

    Basic input file created. Basic file reader implimented.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,27 @@
 package com.company;
 
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 	// write your code here
-        System.out.println("hello world");
+        System.out.println("Hello World");
+
+        // Passing the path to the file as a parameter
+        File testFile = new File(
+                "B:\\OneDrive\\Documents\\CSC 139\\VMM Lab\\Deliverables\\TestInput.txt");
+
+        // Creating an object of BuffferedReader class
+        BufferedReader br = new BufferedReader(new FileReader(testFile));
+
+        String st;
+
+        while ((st = br.readLine() ) != null){
+            // Print the string of each line
+            System.out.println(st);
+        }
+
+
     }
 }
